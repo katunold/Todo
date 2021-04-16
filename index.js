012589584192b8c30a119c65a0aee36236ace8cb
katunold <katunold94@gmail.com>
2021-04-16 13:13:17 +0300

    - create welcome route

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 console.log('Starting Server ....')
 
+const todos = [];
+
 app.listen(3000, 'localhost', () => console.log('Server Started Successfully 🎊'))
+
+app.get('/', (req,res) => {
+  res.send('Welcome to our todo api demo 🎊')
+});
